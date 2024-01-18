22046f44e088ba79b8798940aa42dfaff6cc3286
Melvin <mchevallier@aubay.com>
2024-01-18 16:56:45 +0100

    Cant see nft burned

--- pages/buy.tsx
+++ pages/buy.tsx
@@ -1,5 +1,5 @@
 import { useContract, useNFTs } from "@thirdweb-dev/react";
-import React from "react";
+import React, { useMemo } from "react";
 import Container from "../components/Container/Container";
 import NFTGrid from "../components/NFT/NFTGrid";
 import { NFT_COLLECTION_ADDRESS } from "../const/contractAddresses";
@@ -9,12 +9,22 @@ export default function Buy() {
   const { contract } = useContract(NFT_COLLECTION_ADDRESS);
   const { data, isLoading } = useNFTs(contract);
 
+  // Filter out burned NFTs
+  const unburnedNFTs = useMemo(() => {
+    if (!data) {
+      return [];
+    }
+    // Assuming that a burned NFT has its owner set to the null address
+    const nullAddress = "0x0000000000000000000000000000000000000000";
+    return data.filter(nft => nft.owner !== nullAddress);
+  }, [data]);
+
   return (
     <Container maxWidth="lg">
       <h1>Buy NFTs</h1>
       <p>Browse which NFTs are available from the collection.</p>
       <NFTGrid
-        data={data}
+        data={unburnedNFTs}
         isLoading={isLoading}
         emptyText={
           "Looks like there are no NFTs in this collection. Did you import your contract on the thirdweb dashboard? https://thirdweb.com/dashboard"
